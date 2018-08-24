9c02f5a61ae705e03a0daa24b24024737165389d
Kunal Ganglani <kunalganglani@gmail.com>
2018-08-24 12:58:07 +0530

    data call to api made

--- src/app/app.js
+++ src/app/app.js
@@ -23,7 +23,18 @@ class AppCtrl {
         count: response.data.length,
         data: response.data
       };
+      var idArr = $scope.tableData.data.map(item => item.id);
+      getMoreDetails(idArr);
     });
+    function getMoreDetails(idArr) {
+      var firstFive = idArr.splice(0,5);
+      $http.get(`https://td-predictions-staging.herokuapp.com/fire-quit-in-6m/2018-06-01/?user_ids=${firstFive.join()}`).then(
+          function(response){
+            item.info = response;
+          }
+        )
+    }
+
     $scope.query = {
       _limit: 5,
       _page: 1
