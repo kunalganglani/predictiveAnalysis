0be256c05a8a4c42e582a7fb5b9ef76ed46d8773
Kunal Ganglani <kunalganglani@gmail.com>
2018-08-26 11:12:04 +0530

    error handler added, user service added

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 require('angular-chart.js');
 import { AppCtrl } from './app.controller';
+import { getUserDataService } from './service/getUserDataService';
 import { employeeTableController } from './employeeTable.controller';
 import '../style/app.css';
 let app = () => {
@@ -14,9 +15,10 @@ const appModule = 'app';
 angular.module(appModule, [
   'ngMaterial', 'ngRoute', 'md.data.table','chart.js'
 ]).directive('app', app)
+.service('myService', ['$http', '$q', getUserDataService])
   .component('navToolbar', { template: require('./navToolbar/navToolbar.html') })
   .controller('AppCtrl', AppCtrl)
-  .controller('employeeTableController', ['$mdEditDialog', '$q', '$scope', '$http', '$timeout', employeeTableController])
+  .controller('employeeTableController', ['$mdEditDialog', '$q', '$scope', '$http', '$timeout', 'myService', '$mdDialog', employeeTableController])
   .config(['$mdThemingProvider', function ($mdThemingProvider) {
     'use strict';
     $mdThemingProvider.theme('default')
--- src/app/employeeTable.controller.js
+++ src/app/employeeTable.controller.js
@@ -1,97 +1,110 @@
-export function employeeTableController($mdEditDialog, $q, $scope,$http, $timeout) {
-    'use strict';
-    $http.get("http://localhost:3000/users").then(function (response) {
+export function employeeTableController($mdEditDialog, $q, $scope, $http, $timeout, myService, $mdDialog) {
+  'use strict';
+  function onInit() {
+    getUserData();  
+  }
+  onInit();
+  function getUserData() {
+    $scope.success = false,
+    $scope.error = false;
+    let successHandler = function (response) {
       $scope.tableData = {
         count: response.data.length,
         data: response.data
       };
-      var idArr = $scope.tableData.data.map(item => item.id);
-      getMoreDetails(idArr);
-    });
-    function getMoreDetails(idArr) {
-      var firstFive = idArr.splice(0,5);
-      $http.get(`https://td-predictions-staging.herokuapp.com/fire-quit-in-6m/2018-06-01/?user_ids=${firstFive.join()}`).then(
-          function(response){
-            console.log($scope.tableData);
-            console.log(response);
-          }
-        )
-    }
-    $scope.colors = ["rgb(159,204,0)","rgb(250,109,33)"];          
-    $scope.labels = ["Fire", "Quit"];
-    $scope.data = [20,80];
-    
-    $scope.selected = [];
-    $scope.limitOptions = [5, 10, 15];
-    
-    $scope.options = {
-      rowSelection: false,
-      multiSelect: true,
-      autoSelect: true,
-      decapitate: false,
-      largeEditDialog: false,
-      boundaryLinks: true,
-      limitSelect: true,
-      pageSelect: true
-    };
-    
-    $scope.query = {
-      order: 'name',
-      limit: 5,
-      page: 1
-    };
-    
-    $scope.editComment = function (event, employee) {
-      event.stopPropagation(); // in case autoselect is enabled
-      
-      var editDialog = {
-        modelValue: employee.comment,
-        placeholder: 'Add a comment',
-        save: function (input) {
-          if(input.$modelValue === 'Donald Trump') {
-            input.$invalid = true;
-            return $q.reject();
-          }
-          if(input.$modelValue === 'Bernie Sanders') {
-            return employee.comment = 'FEEL THE BERN!'
-          }
-          employee.comment = input.$modelValue;
-        },
-        targetEvent: event,
-        title: 'Add a comment',
-        validators: {
-          'md-maxlength': 30
-        }
-      };
-      
-      var promise;
-      
-      if($scope.options.largeEditDialog) {
-        promise = $mdEditDialog.large(editDialog);
-      } else {
-        promise = $mdEditDialog.small(editDialog);
-      }
-      
-      promise.then(function (ctrl) {
-        var input = ctrl.getInput();
-        
-        input.$viewChangeListeners.push(function () {
-          input.$setValidity('test', input.$modelValue !== 'test');
-        });
-      });
-    };
-    
-    $scope.toggleLimitOptions = function () {
-      $scope.limitOptions = $scope.limitOptions ? undefined : [5, 10, 15];
-    };
-    
-    $scope.getTypes = function () {
-      return ['Candy', 'Ice cream', 'Other', 'Pastry'];
-    };
-    
-    $scope.loadStuff = function () {
-      $scope.promise = $timeout(function () {
-        // loading
-      }, 2000);
-    }
+      $scope.success = true;
+    }; 
+    let errorHandler = function (reason) {
+      $scope.somethingWrong = reason.stausText || "Internal Server Error";
+      $scope.error = true;
+      $mdDialog.show(
+        $mdDialog.alert()
+          .clickOutsideToClose(true)
+          .title('Alert')
+          .textContent($scope.somethingWrong)
+          .ariaLabel('Alert Dialog '+ $scope.somethingWrong)
+          .ok('Ok')
+      );
+    }; 
+    let askForPromise = myService.getPromise();
+    askForPromise.then(
+      successHandler,
+      errorHandler
+    )
   }
+  $scope.data = [20,80];
+  $scope.colors = ["rgb(159,204,0)", "rgb(250,109,33)"];
+  $scope.labels = ["Fire", "Quit"];
+  $scope.selected = [];
+  $scope.limitOptions = [5, 10, 15];
+  $scope.options = {
+    rowSelection: false,
+    multiSelect: true,
+    autoSelect: true,
+    decapitate: false,
+    largeEditDialog: false,
+    boundaryLinks: true,
+    limitSelect: true,
+    pageSelect: true
+  };
+  $scope.query = {
+    order: 'name',
+    limit: 5,
+    page: 1
+  };
+
+  $scope.editComment = function (event, employee) {
+    event.stopPropagation(); // in case autoselect is enabled
+
+    var editDialog = {
+      modelValue: employee.comment,
+      placeholder: 'Add a comment',
+      save: function (input) {
+        if (input.$modelValue === 'Donald Trump') {
+          input.$invalid = true;
+          return $q.reject();
+        }
+        if (input.$modelValue === 'Bernie Sanders') {
+          return employee.comment = 'FEEL THE BERN!'
+        }
+        employee.comment = input.$modelValue;
+      },
+      targetEvent: event,
+      title: 'Add a comment',
+      validators: {
+        'md-maxlength': 30
+      }
+    };
+
+    var promise;
+
+    if ($scope.options.largeEditDialog) {
+      promise = $mdEditDialog.large(editDialog);
+    } else {
+      promise = $mdEditDialog.small(editDialog);
+    }
+
+    promise.then(function (ctrl) {
+      var input = ctrl.getInput();
+
+      input.$viewChangeListeners.push(function () {
+        input.$setValidity('test', input.$modelValue !== 'test');
+      });
+    });
+  };
+
+  $scope.toggleLimitOptions = function () {
+    $scope.limitOptions = $scope.limitOptions ? undefined : [5, 10, 15];
+  };
+  $scope.getEmpLeavingProbab = function (emp) {
+    return emp.id;
+  }
+  $scope.getChartData = function (employee) {
+    return [20, 50];
+  }
+  $scope.loadStuff = function () {
+    $scope.promise = $timeout(function () {
+      // loading
+    }, 2000);
+  }
+}
