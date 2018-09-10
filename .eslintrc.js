b31175afb6434651b6f9956a6c0f9e012b50af73
Kunal Ganglani <kunalganglani@gmail.com>
2018-09-10 19:59:20 +0530

    globals added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
+  "globals": {
+    "expect": true,
+    "element": true,
+    "browser": true,
+  },
   "env": {
     "commonjs": true,
     "node": true,
     "mocha": true
   },
-  rules:{
+  rules: {
     "linebreak-style": 0
   },
-    "extends": "airbnb-base"
-  };
+  "extends": "airbnb-base"
+};
